c80a68bf70309aae8b98b75d9bdd76fd3e287d01
George Alton <georgealton@gmail.com>
2021-11-12 21:00:54 +0000

    Fixup some types

--- iam_policy_validate_sarif/run.py
+++ iam_policy_validate_sarif/run.py
@@ -1,14 +1,19 @@
 from . import validator
 from . import converter
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import TextIO
+    from mypy_boto3_accessanalyzer.literals import LocaleType, PolicyTypeType
 
 
 def validate_as_sarif(
-    policy_location,
-    policy_document,
-    policy_type,
-    locale,
-    output_location,
-):
+    policy_location: str,
+    policy_document: str,
+    policy_type: PolicyTypeType,
+    locale: LocaleType,
+    output_location: TextIO,
+) -> None:
     findings = validator.validate(
         policy_type=policy_type, locale=locale, policy=policy_document
     )
--- iam_policy_validate_sarif/validator.py
+++ iam_policy_validate_sarif/validator.py
@@ -1,7 +1,16 @@
 import boto3
+from typing import TYPE_CHECKING
 
 
-def validate(locale, policy, policy_type):
+if TYPE_CHECKING:
+    from mypy_boto3_accessanalyzer.literals import LocaleType, PolicyTypeType
+    from mypy_boto3_accessanalyzer.type_defs import ValidatePolicyFindingTypeDef
+    from typing import Iterable
+
+
+def validate(
+    locale: LocaleType, policy: str, policy_type: PolicyTypeType
+) -> "Iterable[ValidatePolicyFindingTypeDef]":
     client = boto3.client("accessanalyzer")
     paginator = client.get_paginator("validate_policy")
     pages = paginator.paginate(
