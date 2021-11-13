a2423d45627992ef97bf1b2bf32c46fa49b5f3f7
George Alton <georgealton@gmail.com>
2021-11-13 00:50:42 +0000

    move policies

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,5 +1,5 @@
 import json
-import pathlib
+from pathlib import Path
 
 import jsonschema
 import pytest
@@ -10,26 +10,27 @@ from iam_policy_validate_sarif import converter
 @pytest.mark.parametrize(
     "policy",
     [
-        ("arn-region-not-allowed.policy.json"),
-        ("redundant-action.policy.json"),
+        ("arn-region-not-allowed"),
+        ("redundant-action"),
     ],
 )
 def test_convertor(policy):
-    policy_path = f"tests/data/policy_checks/{policy}"
-    sarif_path = f"tests/data/policy_checks/sarif/{policy}"
+    base_path = Path("tests/data/policy_checks")
+    policy_path = Path(base_path, "policies/", f"{policy}.json")
+    findings_path = Path(base_path, "findings/", f"{policy}.json")
+    sarif_path = Path(base_path, "sarif/", f"{policy}.sarif")
 
     with open("tests/data/sarif-schema-2.1.0.json") as schema_file:
         schema = json.load(schema_file)
-
-    with open(f"{policy_path}.findings") as data:
+    with open(findings_path) as data:
         findings = json.load(data)["findings"]
+    with open(sarif_path) as data:
+        expected_sarif = json.load(data)
 
-    sarif_converter = converter.SarifConverter(pathlib.Path(policy_path))
+    sarif_converter = converter.SarifConverter(policy_path)
     sarif = json.loads(sarif_converter.convert(findings))
     print(json.dumps(sarif))
 
-    with open(f"{sarif_path}.findings.sarif") as data:
-        expected_sarif = json.load(data)
 
     assert sarif == expected_sarif
     jsonschema.validate(sarif, schema)
