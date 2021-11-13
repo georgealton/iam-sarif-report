f183068d0f653dd1154c84a3a325098139bd5037
George Alton <georgealton@gmail.com>
2021-11-13 00:27:41 +0000

    Add results writing

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
-function find_policies(){ find $1 -type f -maxdepth 1; }
+result_path=$4
+result=$4
+function find_policies(){ find "$1" -type f -maxdepth 1; }
 for policy in $(find_policies "$3"); do
-    result=$4/$(basename "$policy").sarif
+    if [[ "$result_path" != "-" ]]; then
+        mkdir -p "$result_path"
+        result="$result_path/$(basename $policy).sarif"
+    fi
     iam-policy-validator-to-sarif \
         --policy-type "$1" \
         --locale "$2" \
-        "$policy"
+        -- \
+        "$policy" \
+        "$result"
 done
