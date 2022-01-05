99711fea92e9606c4f0f0cb5358d2a33c20ea5da
George Alton <georgealton@gmail.com>
2022-01-05 00:17:10 +0000

    remove context manager

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -15,22 +15,19 @@ from iam_policy_validate_sarif import converter
     ],
 )
 def test_convertor(policy):
+    schema_path = Path("tests/data/sarif-schema-2.1.0.json")
     base_path = Path("tests/data/policy_checks")
     policy_path = Path(base_path, "policies/", f"{policy}.json")
     findings_path = Path(base_path, "findings/", f"{policy}.json")
     sarif_path = Path(base_path, "sarif/", f"{policy}.sarif")
 
-    with open("tests/data/sarif-schema-2.1.0.json") as schema_file:
-        schema = json.load(schema_file)
-    with open(findings_path) as data:
-        findings = json.load(data)["findings"]
-    with open(sarif_path) as data:
-        expected_sarif = json.load(data)
+    sarif_schema = json.loads(schema_path.read_text())
+    findings = json.loads(findings_path.read_text())["findings"]
+    expected_sarif = json.loads(sarif_path.read_text())
 
     sarif_converter = converter.SarifConverter(policy_path)
     sarif = json.loads(sarif_converter.convert(findings))
     print(json.dumps(sarif))
 
-
     assert sarif == expected_sarif
-    jsonschema.validate(sarif, schema)
+    jsonschema.validate(sarif, sarif_schema)
