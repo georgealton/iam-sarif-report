3e3ede7208ebb8a09940cbf94876d273b597d876
George Alton <georgealton@gmail.com>
2022-01-14 23:53:17 +0000

    sdfvscv

--- scripts/generate_findings.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-find 'tests/data' \
-    -name '*.policy.json' \
-    -type f \
-    -exec sh -c 'aws accessanalyzer validate-policy --policy-document file://{} --policy-type IDENTITY_POLICY > {}.findings' \;
