11593dbb7333150c9f8462dd6db4d709f715c4da
George Alton <georgealton@gmail.com>
2023-02-23 13:45:16 +0000

    fix noxfile

--- noxfile.py
+++ noxfile.py
@@ -10,13 +10,14 @@ python_versions = [
     "3.11",
 ]
 
+
 @nox.session(python=python_versions)
 def test(session):
     session.install(".[test]")
     session.run("pytest", "-v")
 
+
 @nox.session(python=base_python)
 def pre_commit(session):
     session.install("pre-commit")
     session.run("pre-commit", "run", "--all-files")
-
