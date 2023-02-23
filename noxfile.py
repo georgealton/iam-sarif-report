5bcdde02c3a594d1725c26c56b175f9ec1724ae6
George Alton <georgealton@gmail.com>
2023-02-23 23:31:51 +0000

    nox-types

--- noxfile.py
+++ noxfile.py
@@ -5,13 +5,15 @@ BASE_PYTHON = "3.11"
 ALL_PYTHON_VERSIONS = ("3.7", "3.8", "3.9", "3.10", "3.11")
 
 
-@nox.session(python=ALL_PYTHON_VERSIONS)
+@nox.session(python=ALL_PYTHON_VERSIONS, tags=["tests"])
 def test(session: Session) -> None:
+    """Run Tests."""
     session.install(".[test]")
     session.run("pytest", "-v")
 
 
-@nox.session(python=BASE_PYTHON)
-def pre_commit(session: Session):
+@nox.session(python=BASE_PYTHON, tags=["lint"])
+def pre_commit(session: Session) -> None:
+    """Run pre-commit."""
     session.install("pre-commit")
     session.run("pre-commit", "run", "--all-files")
