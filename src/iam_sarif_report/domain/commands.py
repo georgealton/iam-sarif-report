d552df4795fadb18bb69b0b56b24687c83f34e1c
George Alton <galton@evertz.com>
2024-02-28 16:57:14 +0000

    remove uri support

--- src/iam_sarif_report/domain/commands.py
+++ src/iam_sarif_report/domain/commands.py
@@ -5,7 +5,7 @@ from typing import final
 
 from attrs import frozen
 
-from .definitions import LOCALES, POLICY_TYPES, RESOURCE_TYPES, URI
+from .definitions import LOCALES, POLICY_TYPES, RESOURCE_TYPES
 
 
 class Command:
@@ -15,7 +15,7 @@ class Command:
 @final
 @frozen(kw_only=True)
 class GenerateFindingsAndReportSarif(Command):
-    policy_locations: list[URI] | URI
+    policy_locations: list[str] | str
     policy_type: POLICY_TYPES
     locale: LOCALES
     resource_type: RESOURCE_TYPES | None
--- src/iam_sarif_report/domain/definitions.py
+++ src/iam_sarif_report/domain/definitions.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 
 from enum import Enum, unique
 
-URI = str
-
 
 class StrEnum(str, Enum):
     def __str__(self) -> str:
